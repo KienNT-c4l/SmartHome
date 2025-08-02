82a486e71cd7ab84b82cbeb5268e6c0da51fce22
Duc-HUST-ET1 <duc.ta233337@sis.hust.edu.vn>
2025-08-02 22:10:20 +0700

    remove HiveMQ.py

--- HiveMQ.py
+++
@@ -1,8 +0,0 @@
-class HiveMQ_Info:
-    broker = 'broker.emqx.io'
-    port = 1883
-    topic = 'home/temperature'  # sửa nếu topic khác
-    client_id = 'smarthome-rules-anhduc'
-    TEMP_THRESHOLD = 40.0  # °C
-
-    
