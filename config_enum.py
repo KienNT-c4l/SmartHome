980526dd412c43f5f449f4a76e52742351da4432
Duc-HUST-ET1 <duc.ta233337@sis.hust.edu.vn>
2025-08-03 08:58:50 +0700

    Change topic

--- config_enum.py
+++ config_enum.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 class MQTT_Info:
     broker = "broker.emqx.io"
     port = 1883
-    topic = 'home/temperature'  # sửa nếu topic khác
+    topic = 'home/kitchen/temperature'  # sửa nếu topic khác
     client_id = 'smarthome-rules'
 
 class THRESOLD:
