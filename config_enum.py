e3d4f628ed60aa07d8dd707b9a0a423d318cf27a
Nguyễn Trung Kiên <112324296+KienNT-c4l@users.noreply.github.com>
2025-08-13 01:34:47 +0700

    kitchen

---
+++ config_enum.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def get_ipv4_address():
+    import socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
+    except Exception as e:
+        print(f"Error getting IPv4 address: {e}")
+        return None
+
+class MQTT_Info:
+    broker_main = get_ipv4_address()
+    broker_fallback = 'broker.emqx.io'
+    port = 1883
+    topic_temperature = '/home/kitchen/temperature' 
+    topic_humidity = '/home/kitchen/humidity'
+    topic_gas = '/home/kitchen/gas'
+    client_id = 'smarthome-rules'
+
+class THRESOLD:
+    TEMP_THRESHOLD = 40.0  # °C
+    GAS_THRESHOLD = 100
+    
+
+
+    
---
+++ notify.py
@@ -0,0 +1,20 @@
+from telegram import Bot
+from telegram.constants import ParseMode
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+bot = Bot(token=BOT_TOKEN)
+
+async def send_telegram_alert(message):
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=ParseMode.HTML)
+        print("✅ Đã gửi cảnh báo Telegram")
+    except Exception as e:
+        print("❌ Lỗi gửi Telegram:", e)
