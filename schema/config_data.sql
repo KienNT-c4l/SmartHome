5cb595c816727e77cff40d6b145e13eefde55aed
Võ Hồng Phúc <hphuc15@outlook.com.vn>
2025-08-17 21:42:46 +0700

    Delete schema directory
    Thừa

--- schema/config_data.sql
+++
@@ -1,7 +0,0 @@
-USE smarthome;
-
--- Add measurement units
-INSERT INTO units (name, symbol, description) VALUES 
-('Temperature', '°C', 'Celsius degree'),
-('Humidity', '%', 'Relative Humidity'),
-('Gas_level', 'ppm', 'Parts per million - gas concentration');
